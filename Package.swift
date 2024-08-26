6009c9a7e1c18529c4440fb43641600e6fa84c90
Anish Doshi <apdoshi@berkeley.edu>
2024-08-26 11:33:20 -0700

    ios14 in core

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "helium-swift",
     platforms: [
-        .iOS(.v15), .macOS(.v11)
+        .iOS(.v14), .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
