51ee751d59c88d19a7720d18b367f4776709b861
GitHub Action <action@github.com>
2024-11-13 04:16:46 +0000

    chore: update version to v1.4.9-qa

--- Sources/HeliumCore/BuildConstants.swift
+++ Sources/HeliumCore/BuildConstants.swift
@@ -1,7 +1,7 @@
 public struct BuildConstants {
     /// Current SDK version
     /// This is automatically updated by GitHub Actions on release
-    public static let version = "1.4.8-qa-3"
+    public static let version = "1.4.9-qa"
     
     /// Get the current SDK version
     public static func current() -> String {
