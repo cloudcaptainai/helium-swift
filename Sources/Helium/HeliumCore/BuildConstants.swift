9790873fee93c9b99030ecfb915063c5be4db3ea
GitHub Action <action@github.com>
2025-03-02 02:16:13 +0000

    chore: update version to 1.5.9

--- Sources/Helium/HeliumCore/BuildConstants.swift
+++ Sources/Helium/HeliumCore/BuildConstants.swift
@@ -1,7 +1,7 @@
 public struct BuildConstants {
     /// Current SDK version
     /// This is automatically updated by GitHub Actions on release
-    public static let version = "1.5.9-qa"
+    public static let version = "1.5.9"
     
     /// Get the current SDK version
     public static func current() -> String {
