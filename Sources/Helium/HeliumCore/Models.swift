8b1d09c748a8bc2da12aee5494cfa76f25fadcca
Kyle Gorlick <kyle@kylegorlick.com>
2025-04-05 18:10:49 -0700

    Remove Segment usage and use SwiftyJSON (correct JSON usage)

--- Sources/Helium/HeliumCore/Models.swift
+++ Sources/Helium/HeliumCore/Models.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 import SwiftUI
 import AnyCodable
-import Segment
+import SwiftyJSON
 
 public struct HeliumPaywallInfo: Codable {
     public init(paywallID: Int, paywallTemplateName: String, productsOffered: [String], resolvedConfig: AnyCodable, shouldShow: Bool, fallbackPaywallName: String, experimentID: String? = nil, modelID: String? = nil, resolvedConfigJSON: JSON? = nil, forceShowFallback: Bool? = false) {
