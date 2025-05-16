45e84b6d7e0afcfb4d62368bbf6bb2aab5e97beb
Kyle Gorlick <kyle@kylegorlick.com>
2025-05-15 18:11:22 -0700

    Adjust BuildConstants with more comments and test full flow

--- Sources/Helium/HeliumCore/BuildConstants.swift
+++ Sources/Helium/HeliumCore/BuildConstants.swift
@@ -1,6 +1,10 @@
 /**
  Note that this file is the source of truth for package/pod version, and GitHub actions rely on it. Do not change this file
- at all other than to update the version.
+ at all other than to update the version. Adjusting this file wlll kick off a flow to ultimately create a new release.
+ 
+ See CONTRIBUTING.md for full details on the release process and GitHub action workflows.
+ 
+ Note - if you end the version in -pre, it will create as a pre-release.
  */
 public struct BuildConstants {
     /// Current SDK version
