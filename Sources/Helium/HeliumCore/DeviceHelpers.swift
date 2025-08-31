99dfac0ecaf279a1f2854b05cd79380787d9971e
Kyle Gorlick <kyle@kylegorlick.com>
2025-08-31 10:56:14 -0700

    Mention modificitions near DeviceKit copyright

--- Sources/Helium/HeliumCore/DeviceHelpers.swift
+++ Sources/Helium/HeliumCore/DeviceHelpers.swift
@@ -18,7 +18,7 @@
 //OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //THE SOFTWARE.
 
-//NOTE - largely taken from https://github.com/devicekit/DeviceKit
+//NOTE - largely taken from https://github.com/devicekit/DeviceKit with some minor modifications
 
 import UIKit
 
