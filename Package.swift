647d8a3ff0204e7de577ef6672545d3b6ec51338
Kyle Gorlick <kyle@kylegorlick.com>
2025-09-01 15:44:01 -0700

    Remove extra SwiftUI import and unnecessary empty dependencies array

--- Package.swift
+++ Package.swift
@@ -24,8 +24,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Helium",
-            dependencies: []
+            name: "Helium"
         ),
         .target(
             name: "HeliumRevenueCat",
