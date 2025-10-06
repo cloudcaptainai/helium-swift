0e283961357760d7f70b69e58045e4cd8d01fe06
Kyle Gorlick <kyle@kylegorlick.com>
2025-10-06 11:32:53 -0700

    Add some doc comments and private(set) showHeliumDialog

--- Sources/Helium/RestorePurchaseConfig.swift
+++ Sources/Helium/RestorePurchaseConfig.swift
@@ -7,17 +7,20 @@
 
 public class RestorePurchaseConfig {
     
-    var showHeliumDialog: Bool = true
+    private(set) var showHeliumDialog: Bool = true
     
     var restoreFailedTitle: String = "Restore Failed"
     var restoreFailedMessage: String = "We couldn't find any previous purchases to restore."
     var restoreFailedCloseButtonText: String = "OK"
     
-    
-    public func disableDefaultRestoreFailedDialog() {
+    /// Disable the default dialog that Helium will display if a "Restore Purchases" action is not successful.
+    /// You can handle this yourself if desired by listening for the PurchaseRestoreFailedEvent.
+    public func disableRestoreFailedDialog() {
         showHeliumDialog = false
     }
     
+    /// Set custom strings to show in the dialog that Helium will display if a "Restore Purchases" action is not successful.
+    /// Note that these strings will not be localized by Helium for you.
     public func setCustomRestoreFailedStrings(
         customTitle: String? = nil,
         customMessage: String? = nil,
