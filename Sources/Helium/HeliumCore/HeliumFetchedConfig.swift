bee58b9f1dc6d08ed7e9ff76a8cee30633b6a50c
Kyle Gorlick <kyle@kylegorlick.com>
2025-10-31 17:28:13 -0700

    -- no timeout (increase to default) on last retry of config/bundles -- decrease delay between product fetches

--- Sources/Helium/HeliumCore/HeliumFetchedConfig.swift
+++ Sources/Helium/HeliumCore/HeliumFetchedConfig.swift
@@ -77,7 +77,8 @@ class NetworkReachability {
 
 func fetchEndpoint(
     endpoint: String,
-    params: [String: Any]
+    params: [String: Any],
+    timeoutInterval: TimeInterval? = nil
 ) async throws -> (HeliumFetchedConfig?, JSON?) {
     let urlString = endpoint
     guard let url = URL(string: urlString) else {
@@ -94,7 +95,7 @@ func fetchEndpoint(
     request.setValue("gzip, deflate", forHTTPHeaderField: "Accept-Encoding")
 //    request.setValue("true", forHTTPHeaderField: "X-Helium-Skip-Bundles") // keep off for now
     
-    request.timeoutInterval = 15
+    request.timeoutInterval = timeoutInterval ?? 15
     
     let jsonData = try JSONSerialization.data(withJSONObject: params, options: [])
     request.httpBody = jsonData
@@ -176,8 +177,9 @@ public class HeliumFetchedConfigManager: ObservableObject {
         completion: @escaping (HeliumFetchResult) -> Void
     ) async {
         do {
-            // Make the request asynchronously
-            let response = try await fetchEndpoint(endpoint: endpoint, params: params)
+            // Increase timeout if on last attempt
+            var timeoutInterval: TimeInterval? = attemptCounter == maxAttempts ? 60 : nil
+            let response = try await fetchEndpoint(endpoint: endpoint, params: params, timeoutInterval: timeoutInterval)
             
             // Ensure we have data
             guard let newConfig = response.0, let newConfigJSON = response.1 else {
@@ -402,6 +404,9 @@ public class HeliumFetchedConfigManager: ObservableObject {
         maxAttempts: Int,
         attemptCounter: Int
     ) async -> BundlesFetchResult {
+        // Increase timeout if on last attempt
+        var timeoutInterval: TimeInterval? = attemptCounter == maxAttempts ? 60 : nil
+        let response = try await fetchEndpoint(endpoint: endpoint, params: params, timeoutInterval: timeoutInterval)
         let result = await fetchBundles(bundleUrlToTriggersMap: bundleUrlToTriggersMap)
         
         if !result.bundleUrlsNotFetched.isEmpty {
@@ -444,7 +449,8 @@ public class HeliumFetchedConfigManager: ObservableObject {
     }
     
     private func fetchBundles(
-        bundleUrlToTriggersMap: [String : [String]]
+        bundleUrlToTriggersMap: [String : [String]],
+        timeoutInterval: TimeInterval? = nil
     ) async -> BundlesFetchResult {
         var bundleUrlsNotFetched: [String] = []
 
@@ -459,7 +465,7 @@ public class HeliumFetchedConfigManager: ObservableObject {
             for (url, triggers) in bundleUrlToTriggersMap {
                 group.addTask {
                     do {
-                        let html = try await self.fetchBundleHTML(from: url, using: session)
+                        let html = try await self.fetchBundleHTML(from: url, using: session, timeoutInterval: timeoutInterval)
                         return SingleBundleFetchResult(url: url, html: html, isBadURL: false)
                     } catch {
                         if let urlError = error as? URLError, urlError.code == .badURL {
@@ -488,13 +494,17 @@ public class HeliumFetchedConfigManager: ObservableObject {
         )
     }
 
-    private func fetchBundleHTML(from urlString: String, using session: URLSession) async throws -> String {
+    private func fetchBundleHTML(
+        from urlString: String,
+        using session: URLSession,
+        timeoutInterval: TimeInterval? = nil
+    ) async throws -> String {
         guard let url = URL(string: urlString) else {
             throw URLError(.badURL)
         }
 
         var request = URLRequest(url: url)
-        request.timeoutInterval = 5
+        request.timeoutInterval = timeoutInterval ?? 5
 
         let (data, response) = try await session.data(for: request)
 
