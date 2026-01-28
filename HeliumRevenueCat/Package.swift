7cc3715fc441eb43ae920bce5e8c146dac8abe48
Ryan VanAlstine <ryan@vcgmobile.com>
2026-01-27 20:46:30 -0500

    Separate revenuecat package from main package file.

---
+++ HeliumRevenueCat/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "HeliumRevenueCat",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "HeliumRevenueCat",
+            targets: ["HeliumRevenueCat"])
+    ],
+    dependencies: [
+        .package(path: ".."),
+        .package(url: "https://github.com/RevenueCat/purchases-ios-spm", .upToNextMajor(from: "5.0.0"))
+    ],
+    targets: [
+        .target(
+            name: "HeliumRevenueCat",
+            dependencies: [
+                .product(name: "Helium", package: "iOS-SDK"),
+                .product(name: "RevenueCat", package: "purchases-ios-spm")
+            ]
+        )
+    ]
+)
--- Package.swift
+++ Package.swift
@@ -13,25 +13,12 @@ let package = Package(
         .library(
             name: "Helium",
             targets: ["Helium"]),
-        .library(
-            name: "HeliumRevenueCat",
-            targets: ["HeliumRevenueCat", "Helium"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/RevenueCat/purchases-ios-spm", .upToNextMajor(from: "5.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Helium"
-        ),
-        .target(
-            name: "HeliumRevenueCat",
-            dependencies: [
-                "Helium",
-                .product(name: "RevenueCat", package: "purchases-ios-spm")
-            ]
         ),
         .testTarget(
             name: "helium-swiftTests",
