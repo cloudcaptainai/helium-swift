d5dc2833126c5a2f57f09bbac980016b227bad2c
Kyle Gorlick <kyle@kylegorlick.com>
2026-04-20 13:33:21 -0700

    move user id checks for third party entitlement check since performFetch always called by fetchFromServer

--- Sources/Helium/HeliumCore/StripeCheckout/HeliumPaymentEntitlementsSource.swift
+++ Sources/Helium/HeliumCore/StripeCheckout/HeliumPaymentEntitlementsSource.swift
@@ -126,6 +126,13 @@ open class HeliumPaymentEntitlementsSource: ThirdPartyEntitlementsSource, @unche
     }
 
     private func fetchFromServer(forceNew: Bool = false) async {
+        guard Helium.identify.userId != nil
+                || Helium.identify.revenueCatAppUserId != nil
+                || provider.getCustomerId() != nil else {
+            // We can safely assume entitlements result will be empty if no stripe/paddle customer id and no id
+            // set by host app that may persist across app installs / device change.
+            return
+        }
         let (task, myId): (Task<Void, Never>, UInt) = lock.withLock {
             if !forceNew, let existing = currentFetchTask {
                 return (existing, fetchId)
@@ -146,13 +153,6 @@ open class HeliumPaymentEntitlementsSource: ThirdPartyEntitlementsSource, @unche
     }
 
     private func performFetch() async {
-        guard Helium.identify.userId != nil
-                || Helium.identify.revenueCatAppUserId != nil
-                || provider.getCustomerId() != nil else {
-            // We can safely assume entitlements result will be empty if no stripe/paddle customer id and no id
-            // set by host app that may persist across app installs / device change.
-            return
-        }
         do {
             let body = try HeliumPaymentAPIClient.shared.baseRequestBody(provider: provider)
             let response: PaymentEntitlementResponse = try await HeliumPaymentAPIClient.shared.post(provider.checkEntitlementPath, body: body)
